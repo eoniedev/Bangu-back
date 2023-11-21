9b31e7160839820cf5cabb62841c5ac2fce45d84
eoniedev <eoniedev@gmail.com>
2023-11-21 21:00:35 +0900

    feat: options table ddl 추가

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,100 +1,134 @@
-CREATE TABLE `board` (
-                         `article_no` int NOT NULL AUTO_INCREMENT,
-                         `user_id` varchar(16) DEFAULT NULL,
-                         `subject` varchar(100) DEFAULT NULL,
-                         `content` varchar(2000) DEFAULT NULL,
-                         `hit` int DEFAULT '0',
-                         `register_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                         PRIMARY KEY (`article_no`),
-                         KEY `board_to_members_user_id_fk` (`user_id`),
-                         CONSTRAINT `board_to_members_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `members` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=640 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+create table whereismyhome.dongcode
+(
+    dongCode  varchar(10) not null
+        primary key,
+    sidoName  varchar(30) null,
+    gugunName varchar(30) null,
+    dongName  varchar(30) null
+);
 
-CREATE TABLE `dongcode` (
-                            `dongCode` varchar(10) NOT NULL,
-                            `sidoName` varchar(30) DEFAULT NULL,
-                            `gugunName` varchar(30) DEFAULT NULL,
-                            `dongName` varchar(30) DEFAULT NULL,
-                            PRIMARY KEY (`dongCode`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+create table whereismyhome.houseinfo
+(
+    aptCode              bigint      not null
+        primary key,
+    buildYear            int         null,
+    roadName             varchar(40) null,
+    roadNameBonbun       varchar(5)  null,
+    roadNameBubun        varchar(5)  null,
+    roadNameSeq          varchar(2)  null,
+    roadNameBasementCode varchar(1)  null,
+    roadNameCode         varchar(7)  null,
+    dong                 varchar(40) null,
+    bonbun               varchar(4)  null,
+    bubun                varchar(4)  null,
+    sigunguCode          varchar(5)  null,
+    eubmyundongCode      varchar(5)  null,
+    dongCode             varchar(10) null,
+    landCode             varchar(1)  null,
+    apartmentName        varchar(40) null,
+    jibun                varchar(10) null,
+    lng                  varchar(30) null,
+    lat                  varchar(30) null,
+    constraint `UNIQUE`
+        unique (buildYear, apartmentName, jibun, sigunguCode, eubmyundongCode),
+    constraint houseinfo_dongCode_dongcode_dongCode_fk
+        foreign key (dongCode) references whereismyhome.dongcode (dongCode)
+);
 
-CREATE TABLE `file_info` (
-                             `id` bigint NOT NULL AUTO_INCREMENT,
-                             `room_id` bigint DEFAULT NULL,
-                             `save_folder` varchar(45) DEFAULT NULL,
-                             `original_file` varchar(50) DEFAULT NULL,
-                             `save_file` varchar(50) DEFAULT NULL,
-                             `article_no` int DEFAULT NULL,
-                             PRIMARY KEY (`id`),
-                             KEY `file_info_to_rooms_id_fk_idx` (`room_id`),
-                             KEY `file_info_to_board_artice_no_fk_idx` (`article_no`),
-                             CONSTRAINT `file_info_to_board_artice_no_fk` FOREIGN KEY (`article_no`) REFERENCES `board` (`article_no`),
-                             CONSTRAINT `file_info_to_rooms_id_fk` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+create table whereismyhome.housedeal
+(
+    no             bigint      not null
+        primary key,
+    dealAmount     varchar(20) null,
+    dealYear       int         null,
+    dealMonth      int         null,
+    dealDay        int         null,
+    area           varchar(20) null,
+    floor          varchar(4)  null,
+    cancelDealType varchar(1)  null,
+    aptCode        bigint      null,
+    constraint housedeal_aptCode_houseinfo_aptCode_fk
+        foreign key (aptCode) references whereismyhome.houseinfo (aptCode)
+);
 
-CREATE TABLE `housedeal` (
-                             `no` bigint NOT NULL,
-                             `dealAmount` varchar(20) DEFAULT NULL,
-                             `dealYear` int DEFAULT NULL,
-                             `dealMonth` int DEFAULT NULL,
-                             `dealDay` int DEFAULT NULL,
-                             `area` varchar(20) DEFAULT NULL,
-                             `floor` varchar(4) DEFAULT NULL,
-                             `cancelDealType` varchar(1) DEFAULT NULL,
-                             `aptCode` bigint DEFAULT NULL,
-                             PRIMARY KEY (`no`),
-                             KEY `housedeal_aptCode_houseinfo_aptCode_fk_idx` (`aptCode`),
-                             CONSTRAINT `housedeal_aptCode_houseinfo_aptCode_fk` FOREIGN KEY (`aptCode`) REFERENCES `houseinfo` (`aptCode`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+create index housedeal_aptCode_houseinfo_aptCode_fk_idx
+    on whereismyhome.housedeal (aptCode);
 
-CREATE TABLE `houseinfo` (
-                             `aptCode` bigint NOT NULL,
-                             `buildYear` int DEFAULT NULL,
-                             `roadName` varchar(40) DEFAULT NULL,
-                             `roadNameBonbun` varchar(5) DEFAULT NULL,
-                             `roadNameBubun` varchar(5) DEFAULT NULL,
-                             `roadNameSeq` varchar(2) DEFAULT NULL,
-                             `roadNameBasementCode` varchar(1) DEFAULT NULL,
-                             `roadNameCode` varchar(7) DEFAULT NULL,
-                             `dong` varchar(40) DEFAULT NULL,
-                             `bonbun` varchar(4) DEFAULT NULL,
-                             `bubun` varchar(4) DEFAULT NULL,
-                             `sigunguCode` varchar(5) DEFAULT NULL,
-                             `eubmyundongCode` varchar(5) DEFAULT NULL,
-                             `dongCode` varchar(10) DEFAULT NULL,
-                             `landCode` varchar(1) DEFAULT NULL,
-                             `apartmentName` varchar(40) DEFAULT NULL,
-                             `jibun` varchar(10) DEFAULT NULL,
-                             `lng` varchar(30) DEFAULT NULL,
-                             `lat` varchar(30) DEFAULT NULL,
-                             PRIMARY KEY (`aptCode`),
-                             UNIQUE KEY `UNIQUE` (`buildYear`,`apartmentName`,`jibun`,`sigunguCode`,`eubmyundongCode`) /*!80000 INVISIBLE */,
-                             KEY `houseinfo_dongCode_dongcode_dongCode_fk_idx` (`dongCode`) /*!80000 INVISIBLE */,
-                             CONSTRAINT `houseinfo_dongCode_dongcode_dongCode_fk` FOREIGN KEY (`dongCode`) REFERENCES `dongcode` (`dongCode`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+create index houseinfo_dongCode_dongcode_dongCode_fk_idx
+    on whereismyhome.houseinfo (dongCode);
 
-CREATE TABLE `members` (
-                           `user_id` varchar(16) NOT NULL,
-                           `user_name` varchar(20) NOT NULL,
-                           `user_password` varchar(255) NOT NULL,
-                           `email_id` varchar(20) DEFAULT NULL,
-                           `email_domain` varchar(45) DEFAULT NULL,
-                           `join_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                           `token` varchar(1000) DEFAULT NULL,
-                           PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+create table whereismyhome.members
+(
+    user_id       varchar(16)                         not null
+        primary key,
+    user_name     varchar(20)                         not null,
+    user_password varchar(255)                        not null,
+    email_id      varchar(20)                         null,
+    email_domain  varchar(45)                         null,
+    join_date     timestamp default CURRENT_TIMESTAMP not null,
+    token         varchar(1000)                       null
+);
 
-CREATE TABLE `rooms` (
-                         `id` bigint NOT NULL AUTO_INCREMENT,
-                         `lat` varchar(255) NOT NULL,
-                         `lag` varchar(255) NOT NULL,
-                         `comment` varchar(1000) DEFAULT NULL,
-                         `user_id` varchar(16) NOT NULL,
-                         `subject` varchar(255) DEFAULT NULL,
-                         `register_time` timestamp NULL DEFAULT NULL,
-                         `start_date` date DEFAULT NULL,
-                         `end_date` date DEFAULT NULL,
-                         PRIMARY KEY (`id`),
-                         KEY `user_id` (`user_id`),
-                         CONSTRAINT `rooms_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `members` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+create table whereismyhome.board
+(
+    article_no    int auto_increment
+        primary key,
+    user_id       varchar(16)                         null,
+    subject       varchar(100)                        null,
+    content       varchar(2000)                       null,
+    hit           int       default 0                 null,
+    register_time timestamp default CURRENT_TIMESTAMP not null,
+    constraint board_to_members_user_id_fk
+        foreign key (user_id) references whereismyhome.members (user_id)
+);
+
+create table whereismyhome.rooms
+(
+    id            bigint auto_increment
+        primary key,
+    lat           varchar(255)  not null,
+    `lag`         varchar(255)  not null,
+    comment       varchar(1000) null,
+    user_id       varchar(16)   not null,
+    subject       varchar(255)  null,
+    register_time timestamp     null,
+    start_date    date          null,
+    end_date      date          null,
+    constraint rooms_ibfk_1
+        foreign key (user_id) references whereismyhome.members (user_id)
+);
+
+create table whereismyhome.file_info
+(
+    id            bigint auto_increment
+        primary key,
+    room_id       bigint      null,
+    save_folder   varchar(45) null,
+    original_file varchar(50) null,
+    save_file     varchar(50) null,
+    article_no    int         null,
+    constraint file_info_to_board_artice_no_fk
+        foreign key (article_no) references whereismyhome.board (article_no),
+    constraint file_info_to_rooms_id_fk
+        foreign key (room_id) references whereismyhome.rooms (id)
+);
+
+create index file_info_to_board_artice_no_fk_idx
+    on whereismyhome.file_info (article_no);
+
+create index file_info_to_rooms_id_fk_idx
+    on whereismyhome.file_info (room_id);
+
+create table whereismyhome.options
+(
+    id      bigint auto_increment
+        primary key,
+    room_id bigint not null,
+    opt     int    not null,
+    constraint options_rooms_id_fk
+        foreign key (room_id) references whereismyhome.rooms (id)
+);
+
+create index user_id
+    on whereismyhome.rooms (user_id);
+
