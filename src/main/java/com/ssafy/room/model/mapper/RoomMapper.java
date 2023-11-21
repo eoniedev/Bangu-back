e4a51ccc6a95efa6de67edaaf3e6b156beec261c
eoniedev <eoniedev@gmail.com>
2023-11-21 17:48:02 +0900

    fix: 옵션 파라미터 Map에서 Dto로 변경

--- src/main/java/com/ssafy/room/model/mapper/RoomMapper.java
+++ src/main/java/com/ssafy/room/model/mapper/RoomMapper.java
@@ -1,17 +1,17 @@
 package com.ssafy.room.model.mapper;
 
 import com.ssafy.room.model.dto.RoomDto;
+import com.ssafy.room.model.dto.SearchDto;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
-import java.util.Map;
 
 @Mapper
 public interface RoomMapper {
 
     List<RoomDto> listRooms() throws Exception;
 
-    List<RoomDto> listRoomsByOptions(Map<String, String> options) throws Exception;
+    List<RoomDto> listRoomsByOptions(SearchDto options) throws Exception;
 
     void registerRoom(RoomDto roomDto) throws Exception;
 
--- src/main/java/com/ssafy/room/model/service/TransferServiceImpl.java
+++ src/main/java/com/ssafy/room/model/service/TransferServiceImpl.java
@@ -2,12 +2,13 @@ package com.ssafy.room.model.service;
 
 import com.ssafy.board.model.FileInfoDto;
 import com.ssafy.room.model.dto.RoomDto;
+import com.ssafy.room.model.dto.SearchDto;
 import com.ssafy.room.model.mapper.RoomMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Map;
 
 @Service
 @Slf4j
@@ -20,6 +21,7 @@ public class TransferServiceImpl implements TransferService {
 
 
     @Override
+    @Transactional
     public void registerRoom(RoomDto roomDto) throws Exception {
         roomMapper.registerRoom(roomDto);
         List<FileInfoDto> fileInfos = roomDto.getFileInfos();
@@ -34,7 +36,7 @@ public class TransferServiceImpl implements TransferService {
     }
 
     @Override
-    public List<RoomDto> listRoomsByOptions(Map<String, String> options) throws Exception {
+    public List<RoomDto> listRoomsByOptions(SearchDto options) throws Exception {
         return roomMapper.listRoomsByOptions(options);
     }
 
