8ef76e9d6e5987f7041c8dfbcddf166e248e5fe3
eoniedev <eoniedev@gmail.com>
2023-11-23 10:55:04 +0900

    feat: MemberDto Gender Enum 필드 추가

--- src/main/java/com/ssafy/member/model/dto/MemberDto.java
+++ src/main/java/com/ssafy/member/model/dto/MemberDto.java
@@ -1,5 +1,14 @@
 package com.ssafy.member.model.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
 public class MemberDto {
 
     private String userId;
@@ -8,59 +17,6 @@ public class MemberDto {
     private String emailId;
     private String emailDomain;
     private String joinDate;
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getUserPwd() {
-        return userPwd;
-    }
-
-    public void setUserPwd(String userPwd) {
-        this.userPwd = userPwd;
-    }
-
-    public String getEmailId() {
-        return emailId;
-    }
-
-    public void setEmailId(String emailId) {
-        this.emailId = emailId;
-    }
-
-    public String getEmailDomain() {
-        return emailDomain;
-    }
-
-    public void setEmailDomain(String emailDomain) {
-        this.emailDomain = emailDomain;
-    }
-
-    public String getJoinDate() {
-        return joinDate;
-    }
-
-    public void setJoinDate(String joinDate) {
-        this.joinDate = joinDate;
-    }
-
-    @Override
-    public String toString() {
-        return "MemberDto [userId=" + userId + ", userName=" + userName + ", userPwd=" + userPwd + ", emailId="
-                + emailId + ", emailDomain=" + emailDomain + ", joinDate=" + joinDate + "]";
-    }
-
+    private Gender gender;
+    private int genderInt;
 }
