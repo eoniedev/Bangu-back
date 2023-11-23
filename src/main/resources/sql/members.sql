06cf391a2a6285556bed24bfde531d7e07dc2227
eoniedev <eoniedev@gmail.com>
2023-11-23 14:31:22 +0900

    fix: data update

--- src/main/resources/sql/members.sql
+++ src/main/resources/sql/members.sql
@@ -1,12 +1,14 @@
-INSERT INTO whereismyhome.members (user_id, user_name, user_password, email_id, email_domain, join_date, token) VALUES ('admin', '관리자', 'a80ebc7ab1e29097766019818389b2af089d8262a8db4cc05470b03641f6a802', 'admin', 'ssafy.com', '2023-10-31 16:48:39', null);
-INSERT INTO whereismyhome.members (user_id, user_name, user_password, email_id, email_domain, join_date, token) VALUES ('ssafy', '김싸피', 'a80ebc7ab1e29097766019818389b2af089d8262a8db4cc05470b03641f6a802', 'ssafy', 'ssafy.com', '2023-10-31 16:48:04', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJyZWZyZXNoLXRva2VuIiwiaWF0IjoxNzAwMjA5NjYyLCJleHAiOjE3MDAyMDk3MDIsInVzZXJJZCI6InNzYWZ5In0.k4fs9JiJIof9VSvFx0lTx-H-ikjM1XDYdnuc_dW2-Gc');
-INSERT INTO whereismyhome.members (user_id, user_name, user_password, email_id, email_domain, join_date, token) VALUES ('test1', 'Gildong1', 'a80ebc7ab1e29097766019818389b2af089d8262a8db4cc05470b03641f6a802', 'hong1', 'gmail.com', '2023-11-21 22:52:33', null);
-INSERT INTO whereismyhome.members (user_id, user_name, user_password, email_id, email_domain, join_date, token) VALUES ('test10', 'Gildong10', 'a80ebc7ab1e29097766019818389b2af089d8262a8db4cc05470b03641f6a802', 'hong10', 'gmail.com', '2023-11-21 23:35:09', null);
-INSERT INTO whereismyhome.members (user_id, user_name, user_password, email_id, email_domain, join_date, token) VALUES ('test2', 'Gildong2', 'a80ebc7ab1e29097766019818389b2af089d8262a8db4cc05470b03641f6a802', 'hong2', 'gmail.com', '2023-11-21 22:54:39', null);
-INSERT INTO whereismyhome.members (user_id, user_name, user_password, email_id, email_domain, join_date, token) VALUES ('test3', 'Gildong3', 'a80ebc7ab1e29097766019818389b2af089d8262a8db4cc05470b03641f6a802', 'hong3', 'gmail.com', '2023-11-21 22:54:46', null);
-INSERT INTO whereismyhome.members (user_id, user_name, user_password, email_id, email_domain, join_date, token) VALUES ('test4', 'Gildong4', 'a80ebc7ab1e29097766019818389b2af089d8262a8db4cc05470b03641f6a802', 'hong4', 'gmail.com', '2023-11-21 22:54:51', null);
-INSERT INTO whereismyhome.members (user_id, user_name, user_password, email_id, email_domain, join_date, token) VALUES ('test5', 'Gildong5', 'a80ebc7ab1e29097766019818389b2af089d8262a8db4cc05470b03641f6a802', 'hong5', 'gmail.com', '2023-11-21 22:54:56', null);
-INSERT INTO whereismyhome.members (user_id, user_name, user_password, email_id, email_domain, join_date, token) VALUES ('test6', 'Gildong6', 'a80ebc7ab1e29097766019818389b2af089d8262a8db4cc05470b03641f6a802', 'hong6', 'gmail.com', '2023-11-21 22:55:02', null);
-INSERT INTO whereismyhome.members (user_id, user_name, user_password, email_id, email_domain, join_date, token) VALUES ('test7', 'Gildong7', 'a80ebc7ab1e29097766019818389b2af089d8262a8db4cc05470b03641f6a802', 'hong7', 'gmail.com', '2023-11-21 22:55:08', null);
-INSERT INTO whereismyhome.members (user_id, user_name, user_password, email_id, email_domain, join_date, token) VALUES ('test8', 'Gildong8', 'a80ebc7ab1e29097766019818389b2af089d8262a8db4cc05470b03641f6a802', 'hong8', 'gmail.com', '2023-11-21 22:55:13', null);
-INSERT INTO whereismyhome.members (user_id, user_name, user_password, email_id, email_domain, join_date, token) VALUES ('test9', 'Gildong9', 'a80ebc7ab1e29097766019818389b2af089d8262a8db4cc05470b03641f6a802', 'hong9', 'gmail.com', '2023-11-21 22:55:18', null);
+INSERT INTO `members` VALUES
+('admin','관리자','a80ebc7ab1e29097766019818389b2af089d8262a8db4cc05470b03641f6a802','admin','ssafy.com','2023-10-31 07:48:39',NULL,0),
+('hello','kim','a80ebc7ab1e29097766019818389b2af089d8262a8db4cc05470b03641f6a802','kim','gmail.com','2023-11-23 01:53:32',NULL,1),
+('ssafy','김싸피','a80ebc7ab1e29097766019818389b2af089d8262a8db4cc05470b03641f6a802','ssafy','ssafy.com','2023-10-31 07:48:04',NULL,0),
+('test1','Gildong1','a80ebc7ab1e29097766019818389b2af089d8262a8db4cc05470b03641f6a802','hong1','gmail.com','2023-11-21 13:52:33',NULL,0),
+('test10','Gildong10','a80ebc7ab1e29097766019818389b2af089d8262a8db4cc05470b03641f6a802','hong10','gmail.com','2023-11-21 14:35:09',NULL,0),
+('test2','Gildong2','a80ebc7ab1e29097766019818389b2af089d8262a8db4cc05470b03641f6a802','hong2','gmail.com','2023-11-21 13:54:39',NULL,0),
+('test3','Gildong3','a80ebc7ab1e29097766019818389b2af089d8262a8db4cc05470b03641f6a802','hong3','gmail.com','2023-11-21 13:54:46',NULL,1),
+('test4','Gildong4','a80ebc7ab1e29097766019818389b2af089d8262a8db4cc05470b03641f6a802','hong4','gmail.com','2023-11-21 13:54:51',NULL,1),
+('test5','Gildong5','a80ebc7ab1e29097766019818389b2af089d8262a8db4cc05470b03641f6a802','hong5','gmail.com','2023-11-21 13:54:56',NULL,1),
+('test6','Gildong6','a80ebc7ab1e29097766019818389b2af089d8262a8db4cc05470b03641f6a802','hong6','gmail.com','2023-11-21 13:55:02',NULL,1),
+('test7','Gildong7','a80ebc7ab1e29097766019818389b2af089d8262a8db4cc05470b03641f6a802','hong7','gmail.com','2023-11-21 13:55:08',NULL,1),
+('test8','Gildong8','a80ebc7ab1e29097766019818389b2af089d8262a8db4cc05470b03641f6a802','hong8','gmail.com','2023-11-21 13:55:13',NULL,1),
+('test9','Gildong9','a80ebc7ab1e29097766019818389b2af089d8262a8db4cc05470b03641f6a802','hong9','gmail.com','2023-11-21 13:55:18',NULL,0);
